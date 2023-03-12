a18405573038e507975adb1a06c4579b7fba2d8d
Mohammed Meah <mmeah@riverstar.com>
2023-03-12 09:08:23 -0500

    start video

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('FT Online');
+const video = document.getElementById('video')
+
+function startVideo() {
+    navigator.getUserMedia(
+      { video: {} },
+      stream => video.srcObject = stream,
+      err => console.error(err)
+    )
+}
+
+startVideo();
